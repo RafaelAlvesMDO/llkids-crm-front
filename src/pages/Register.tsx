72f1466fa2d7358dc572341e8dc06d8904580f03
Rafael <rafaelalvesmdo_oficial@hotmail.com>
2025-10-04 17:40:32 -0300

    feat: create empty Register.tsx

---
+++ src/pages/Register.tsx
@@ -0,0 +1,9 @@
+const Register: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="font-bold text-6xl text-black">WORKING ON IT</h1>
+    </div>
+  );
+};
+
+export default Register;
