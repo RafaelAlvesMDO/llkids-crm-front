05b171be1f76d7e3bf634eba2ba26883c78907fd
Rafael <rafaelalvesmdo_oficial@hotmail.com>
2025-10-04 17:35:02 -0300

    feat: added DynamicTitles and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-// import { useState } from 'react'
+import { useEffect } from "react";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-function App() {
+const titles: Record<string, string> = {
+  "/": "Home",
+  "/login": "Login",
+  "/register": "Register",
+  // More Routes Here
+};
+
+const DynamicTitle: React.FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.title = titles[location.pathname] || "Home";
+  }, [location]);
+
+  return null;
+};
+
+const AppRoutes: React.FC = () => (
+  <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+  </Routes>
+);
+
+const App: React.FC = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <Router>
+      <DynamicTitle />
+      <AppRoutes />
+    </Router>
   );
-}
+};
 
 export default App;
