ed7e065b3d7bc97095d6a2ada519449da864dfe3
Rafael <rafaelalvesmdo_oficial@hotmail.com>
2026-01-13 01:07:52 -0300

    Feat: Added new Home Page that will be implemented soon

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <div className="font-bold text-3xl flex items-center justify-center">
+        <h1>WORKING IN PROGRESS</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
