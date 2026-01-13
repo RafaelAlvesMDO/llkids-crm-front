ce8d99a92ba94e69be01eb480594226fd7c3442e
Rafael <rafaelalvesmdo_oficial@hotmail.com>
2026-01-12 23:30:16 -0300

    Feat: Created new Register page file

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,18 @@
+import RegisterLogo from "../components/register/RegisterLogo";
+import RegisterForms from "../components/register/RegisterForms";
+
+import React from "react";
+
 const Register: React.FC = () => {
   return (
-    <div className="flex items-center justify-center">
-      <h1 className="font-bold text-6xl text-black">WORKING ON IT</h1>
-    </div>
+    <>
+      <div className="w-screen min-h-screen flex items-center justify-center bg-neutral-100">
+        <div className="flex flex-col items-stretch gap-4 w-full max-w-2xl">
+          <RegisterLogo />
+          <RegisterForms />
+        </div>
+      </div>
+    </>
   );
 };
 
