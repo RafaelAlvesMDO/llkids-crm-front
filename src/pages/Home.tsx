270c5a6b3001fdf0aa50b7862c5fb46a063f82f9
Rafael <rafaelalvesmdo_oficial@hotmail.com>
2026-01-13 05:43:50 -0300

    Feat: Started building the Home Page with the respective components.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import SideBar from "../components/home/SideBar";
+import NavBar from "../components/home/NavBar";
+import CustomerList from "../components/home/CustomerList";
+import AddCustomerButton from "../components/home/AddCustomerButton";
+import FilterList from "../components/home/FilterList";
 
 const Home: React.FC = () => {
   return (
     <>
-      <div className="font-bold text-3xl flex items-center justify-center">
-        <h1>WORKING IN PROGRESS</h1>
+      <div className="flex h-screen bg-neutral-200">
+        <SideBar />
+
+        <div className="flex flex-col flex-1">
+          <NavBar />
+
+          <main className="flex-1 p-6 overflow-auto space-y-4">
+            <div className="flex items-center justify-between">
+              <AddCustomerButton />
+              <FilterList />
+            </div>
+            <CustomerList />
+          </main>
+        </div>
       </div>
     </>
   );
