662f06573de72ddbd53d2ca99343840def686863
Rafael <rafaelalvesmdo_oficial@hotmail.com>
2026-01-25 02:05:13 -0300

    Update: Removed and changed some SideBar icons

--- src/components/home/SideBar.tsx
+++ src/components/home/SideBar.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import {
   HomeIcon,
+  PencilSquareIcon,
   DocumentTextIcon,
   CurrencyDollarIcon,
-  TicketIcon,
   QuestionMarkCircleIcon,
 } from "@heroicons/react/24/outline";
+
+import { useNavigate } from "react-router-dom";
 
 import LLKidsLogo from "../../assets/llkids-logo.png";
 
 const SideBar: React.FC = () => {
+
+  const navigate = useNavigate();
+
   return (
     <aside className="w-20 bg-primary-300 text-white p-4">
       <img
@@ -19,16 +24,20 @@ const SideBar: React.FC = () => {
       />
       <ul className="flex flex-col items-center justify-center space-y-8">
         <li>
-          <HomeIcon className="h-10 w-10 hover:text-neutral-500 transition cursor-pointer" />
+          <HomeIcon onClick={() => 
+          navigate("/")} 
+          className="h-10 w-10 hover:text-neutral-500 transition cursor-pointer" />
+        </li>
+        <li>
+          <PencilSquareIcon 
+          onClick={() => navigate("/child_register")}
+          className="h-10 w-10 hover:text-neutral-500 transition cursor-pointer" />
         </li>
         <li>
           <DocumentTextIcon className="h-10 w-10 hover:text-neutral-500 transition cursor-pointer" />
         </li>
         <li>
           <CurrencyDollarIcon className="h-10 w-10 hover:text-neutral-500 transition cursor-pointer" />
-        </li>
-        <li>
-          <TicketIcon className="h-10 w-10 hover:text-neutral-500 transition cursor-pointer" />
         </li>
         <li>
           <QuestionMarkCircleIcon className="h-10 w-10 hover:text-neutral-500 transition cursor-pointer" />
